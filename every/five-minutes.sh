f027396b4df049c222d655554ae65454f748d613
Flynn <gpg@flyn.ca>
2023-06-12 10:24:16 -0400

    Remove unused command

--- every/five-minutes.sh
+++ every/five-minutes.sh
@@ -8,9 +8,4 @@ function render_user_and_host() {
     echo "$USER@$(hostname)"
 }
 
-function system_info() {
-    echo "Local IPs: $(hostname -I)"
-}
-
 command_to_file "user-and-host" "$(render_user_and_host)"
-command_to_file "system-info" "$(system_info)"
